5e75bddc8cca61df124ab8018f657b3bf26375d0
Ezequiel Drews <ezequiel.drews@meta.com.br>
2016-09-09 09:13:00 -0300

    Exercicio da aula sobre ponteiros com funcao troca

--- main.c
+++ main.c
@@ -1,29 +1,75 @@
-//**************************************************
-//Programa exemplo de utilizacao de ponteiros      *
-//                                                 * 
-//Laboratório de Algoritmo II                      *   
-//                                                 *
-//Autor: Ezequiel Drews                            *   
-//Data: 02/09/2016                                 *
-//**************************************************
+//*************************************************************
+// Programa exemplo de utilização de ponteiros                *
+//                                                            *
+// Laboratório de Algoritmo II                                *
+//                                                            *
+// Autor: Ezequiel L. Drews                                   *
+// Data: 02/09/2016                                           *
+//*************************************************************
 
-//Funcionalidade do progrma
-//Forma de distribuição do programa
+#include <stdio.h>
+#include <stdlib.h>
 
+// criação da função que realiza a troca das posições
+void trocaPosicao(int *p1, int *p2)
+{
+	int aux = 0;
+	aux = *p1;
+	*p1 = *p2;
+	*p2 = aux;
+}
 
-#include<stdio.h>
-
-//váriaveis globais
-int numero;
-int *Ptr_numero;
-
-main()
-{	//exibe mensagem ao usuário
-	printf("Insira um numero");
+main(void)
+{
 	
-	//pega o valor que o usuário inserir e joga no local de memoria da variavel
-	scanf("%d", &numero);
+	// criacao e inicializacao das variaveis
+	int variavel_1 = 1, variavel_2 = 2;	
+	char controle = 'n', descarga;
 	
-	//imprime o numero inserido pelo usuário
-	printf("Ola mundo %i\n", numero);
+	// criacao dos ponteiros
+	int *ponteiro_1, *ponteiro_2;
+	
+	// enquanto
+	while(1)
+	{
+		// inicializacao dos ponteiros
+		ponteiro_1 = &variavel_1; // ponteiro_1 recebe o endereco de variavel_1
+		ponteiro_2 = &variavel_2; // ponteiro_2 recebe o endereco de variavel_2
+		
+		// exibe o valor contido em cada variavel a partir do endereco contido nos ponteiros
+		printf("\nVariavel_1 = %i", *ponteiro_1);
+		printf("\nVariavel_2 = %i", *ponteiro_2);
+		
+		//swap
+		
+		trocaPosicao(ponteiro_1, ponteiro_2);
+		
+		// exibe o valor contido em cada variavel a partir do endereço contido nos ponteiros
+		printf("\n Variavel1 depois do swap: %i", *ponteiro_1);
+		printf("\n Variavel2 depois do swap: %i", *ponteiro_2);
+		
+		// exibe a solicitacao de controle
+		printf("\n\nDeseja finalizar o programa? (s/n)");
+		
+		// efetua a leitura do comando
+		scanf("%c", &controle);
+		
+		// se o comando indicar a finalizacao, finaliza o loop
+		if(controle == 's') break;
+		
+		// exibe a solicitacao de um valor
+		printf("\nDigite um valor para a variavel_1 ");
+		
+		// efetua a leiturado valor e transfere o valor para a variavel_1
+		scanf("%i", ponteiro_1);
+		
+		// exibe a solicitacao de um valor
+		printf("\nDigite um valor para a variavel_2 ");
+		
+		// efetua a leitura do valor e transfere o valor para a variavel_2
+		scanf("%i%c", ponteiro_2, &descarga);
+		//scanf("%i", ponteiro_2, &descarga);
+	}
+	// retorna 0
+	return 0;
 }
