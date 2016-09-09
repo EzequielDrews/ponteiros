c1827b8f3afdc00f0e46f5ff23cde42672696bc1
Ezequiel Drews <ezequiel.drews@meta.com.br>
2016-09-09 09:16:45 -0300

    Exercicio da aula sobre ponteiros com funcao troca atualizado

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 // criação da função que realiza a troca das posições
 void trocaPosicao(int *p1, int *p2)
 {
+	//criação da variavel auxiliar para efetuar a troca
 	int aux = 0;
 	aux = *p1;
 	*p1 = *p2;
@@ -33,20 +34,20 @@ main(void)
 	while(1)
 	{
 		// inicializacao dos ponteiros
-		ponteiro_1 = &variavel_1; // ponteiro_1 recebe o endereco de variavel_1
-		ponteiro_2 = &variavel_2; // ponteiro_2 recebe o endereco de variavel_2
+		p_1 = &var_1; // ponteiro_1 recebe o endereco de variavel_1
+		p_2 = &var_2; // ponteiro_2 recebe o endereco de variavel_2
 		
 		// exibe o valor contido em cada variavel a partir do endereco contido nos ponteiros
-		printf("\nVariavel_1 = %i", *ponteiro_1);
-		printf("\nVariavel_2 = %i", *ponteiro_2);
+		printf("\nVariavel_1 = %i", *p_1);
+		printf("\nVariavel_2 = %i", *p_2);
 		
 		//swap
 		
-		trocaPosicao(ponteiro_1, ponteiro_2);
+		trocaPosicao(p_1, p_2);
 		
 		// exibe o valor contido em cada variavel a partir do endereço contido nos ponteiros
-		printf("\n Variavel1 depois do swap: %i", *ponteiro_1);
-		printf("\n Variavel2 depois do swap: %i", *ponteiro_2);
+		printf("\n Variavel1 depois do swap: %i", *p_1);
+		printf("\n Variavel2 depois do swap: %i", *p_2);
 		
 		// exibe a solicitacao de controle
 		printf("\n\nDeseja finalizar o programa? (s/n)");
@@ -61,14 +62,14 @@ main(void)
 		printf("\nDigite um valor para a variavel_1 ");
 		
 		// efetua a leiturado valor e transfere o valor para a variavel_1
-		scanf("%i", ponteiro_1);
+		scanf("%i", p_1);
 		
 		// exibe a solicitacao de um valor
 		printf("\nDigite um valor para a variavel_2 ");
 		
 		// efetua a leitura do valor e transfere o valor para a variavel_2
-		scanf("%i%c", ponteiro_2, &descarga);
-		//scanf("%i", ponteiro_2, &descarga);
+		scanf("%i%c", p_2, &descarga);
+		//scanf("%i", p_2, &descarga);
 	}
 	// retorna 0
 	return 0;
