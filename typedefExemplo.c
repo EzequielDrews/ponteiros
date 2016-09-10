27407894afd612abdf2b5692971f81e5b762d7f9
ezequieldrews <ezequiel.losekann@hotmail.com>
2016-09-09 22:17:55 -0300

    Fonte final da aula do dia 09/09, com erros de compilação

--- typedefExemplo.c
+++ typedefExemplo.c
@@ -4,6 +4,8 @@ typedef struct {
 	int Indice;
 	char Nome [50];
 	char Telefone [15];
+//	char endereco [];
+//	char email [];
 } entrada;
 
 int main()
@@ -11,14 +13,24 @@ int main()
 	
 	//criacao e inicializacao das variaveis e ponteiros
 	entrada Pagina_01;
+	entrada *Ptr = &Pagina_01;
 	
-	Pagina_01.Indice = 1;
-	Pagina_01.Nome = {"Ezequiel Losekann Drews"};
-	Pagina_01.Telefone = {"(55) 9903-4995"}
+//	Pagina_01.Indice = 1;
+//	Pagina_01.Nome = {"Ezequiel Losekann Drews"};
+//	Pagina_01.Telefone = {"(55) 9903-4995"}
+
+	printf("\nInsira a entrada: ");
+	scanf("%i", (*Ptr).Indice);
+
+	printf("\nInsira o Nome: ");
+	scanf("%s", &Pagina_01.Nome);
+
+	printf("\nInsira o Telefone: ");
+	scanf("%s", &Pagina_01.Telefone);
 
 		printf("\nPagina: %i", Pagina_01.Indice);
-		printf("\nNome: %i", Pagina_01.Nome);
-		printf("\nTelefone: %i", Pagina_01.Telefone);
+		printf("\nNome: %s", Pagina_01.Nome);
+		printf("\nTelefone: %s", Pagina_01.Telefone);
 		
 	
 	//retorna 0
